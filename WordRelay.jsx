d81cb5b0e228a8e9c8e2ecae4381301306c3e8f7
배문형 <m00nlygreat@baemunhyeong-ui-MacBookPro.local>
2021-01-25 10:30:25 +0900

    끝말잇기 및 webpack 설정 완료

--- WordRelay.jsx
+++ WordRelay.jsx
@@ -3,13 +3,52 @@ const { Component } = React;
 
 class WordRelay extends Component {
   state = {
-    text: "Hello, webpack",
+    word: "제로초",
+    value: "",
+    result: "",
   };
 
-  render() {
-      return <h1>{this.state.text}</h1>
+  onSubmitForm = (e) => {
+    e.preventDefault();
+
+    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+      this.setState({
+        result: '딩동댕!',
+        word: this.state.value,
+        value: '',
+      });
+      this.input.focus();
+    } else {
+      this.setState({
+        result: '땡',
+        value: '',
+      });
+      this.input.focus();
+    }
   }
 
+  onChangeInput = (e) => {
+    this.setState({ value: e.target.value });
+  }
+
+  input;
+  onRefInput = (c) => {
+    this.input = c;
+  }
+
+
+  render() {
+    return (
+      <>
+        <div>{this.state.word}</div>
+        <form action="" onSubmit={this.onSubmitForm}>
+          <input type="text" ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput}/>
+          <button>입력!</button>
+        </form>
+        <div>{this.state.result}</div>
+      </>
+    )
+  }
 }
 
 module.exports = WordRelay;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const { webpack } = require("webpack");
 
 module.exports = {
   name: "word-relay-setting",
@@ -18,12 +19,17 @@ module.exports = {
           test: /\.jsx?/,
           loader: 'babel-loader',
           options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'],
+              presets: [['@babel/preset-env', {
+                targets: {
+                  browsers: ['> 1% in KR'],
+                },
+              }], '@babel/preset-react'],
               plugins: ['@babel/plugin-proposal-class-properties'],
           }
       }]
 
   },
+  
   
   output: {
     path: path.join(__dirname, "dist"),
@@ -39,7 +45,11 @@ module.exports = {
 // mode: 'development'는 실 서비스에서는 production으로 변경.
 
 // @babel/core
-// @babel/preset-env 환경에 맞게 알아서 바꿔줌
+// @babel/preset-env 환경에 맞게 알아서 바꿔줌 (브라우저 버전 등.)
+// preset은 plugin 들의 모음. preset만의 설정에 접근하려면, preset-env를 배열로 감싸고, 2번째 배열요소에 객체로 설정 첨부.
+  // https://github.com/browserslist/browserslist
+  // browsers list는 바벨의 플러그인으로, 자연어 형태의 버전 프리셋을 제공하여, 대응 브라우저 버전을 편하게 설정할 수 있다.
+  // plugins, rules 등이 너무 많아서 뭔지 모르겠다면, 최대한 제거하고 에러메시지를 탐색해가면서 하나씩 추가해본다.
 // @babel/preset-react JSX 사용을 가능케해줌.
 // babel-loader 웹팩과 바벨을 연결해줌.
 
