ad7a988f8c157073d837f5c0b4271ef183d66aad
m00nlygreat <moonlygreat@gmail.com>
2021-01-29 00:10:40 +0900

    반응속도 체크

--- client.jsx
+++ client.jsx
@@ -1,9 +1,8 @@
 const React = require("react");
 const ReactDom = require("react-dom");
-import NumberBaseball from './NumberBaseball'
-// const NumberBaseball = require('./NumberBaseball')
+import ResponseCheck from './ResponseCheck'
 
 
-ReactDom.render(<NumberBaseball />, document.querySelector("#root"));
+ReactDom.render(<ResponseCheck />, document.querySelector("#root"));
 
 // JSX 문법을 쓰고 싶으면 jsx 확장자로
