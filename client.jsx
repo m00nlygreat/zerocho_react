9c8ca57277ac32e1da884eeb4d957a67e0cd4cc6
배문형 <m00nlygreat@baemunhyeong-ui-MacBookPro.local>
2021-02-19 10:56:16 +0900

    로또추첨 레이아웃

--- client.jsx
+++ client.jsx
@@ -1,8 +1,8 @@
 const React = require("react");
 const ReactDom = require("react-dom");
-import RSP from './RSP'
+import Lotto from './Lotto'
 
 
-ReactDom.render(<RSP />, document.querySelector("#root"));
+ReactDom.render(<Lotto />, document.querySelector("#root"));
 
 // JSX 문법을 쓰고 싶으면 jsx 확장자로
