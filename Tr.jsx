6293a06883098d1db89ed73277e9f59e2c69c7d4
배문형 <m00nlygreat@baemunhyeong-ui-MacBookPro.local>
2021-02-23 10:21:24 +0900

    미친 reducer 이거 docs 읽어봐야해

--- Tr.jsx
+++ Tr.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Td from "./Td";
 
 const Tr = ( {rowData} ) => {
-  console.log(rowData.length);
   return <tr>
     {Array(rowData.length).fill().map((td)=><Td />)}
   </tr>;
