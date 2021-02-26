01e3b2dc13676b6256b38ee64381a7530d156b5d
m00nlygreat <moonlygreat@gmail.com>
2021-02-27 04:06:11 +0900

    테이블 최적화 직전까지

--- Td.jsx
+++ Td.jsx
@@ -1,13 +1,17 @@
 import React, {useCallback} from "react";
-import { CHANGE_TURN, CLICK_CELL } from "./TicTacToe";
+import { CLICK_CELL } from "./TicTacToe";
 
 const Td = ({rowIndex, cellIndex, dispatch, cellData}) => {
 
   const onClickTd = useCallback(() => {
-    console.log(rowIndex, cellIndex);
+
+
+    if (cellData) {
+      return;
+    }
+
     dispatch({ type: CLICK_CELL, row: rowIndex, cell: cellIndex});
-    dispatch({ type: CHANGE_TURN});
-  }, []);
+  }, [cellData]);
 
   return <td onClick={onClickTd}>{cellData}</td>;
 };
--- TicTacToe.jsx
+++ TicTacToe.jsx
@@ -1,57 +1,139 @@
-import React, {useState, useReducer, useCallback} from 'react';
-import Table from './Table';
+import React, { useReducer, useCallback, useEffect } from "react";
+import Table from "./Table";
 
 const initialState = {
-    winner: '',
-    turn: 'O',
-    tableData: [['','',''],['','',''],['','','']],
+  winner: "",
+  turn: "O",
+  tableData: [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ],
+  recentCell: [-1, -1],
 };
 
-export const SET_WINNER = 'SET_WINNER';
-export const CLICK_CELL = 'CLICK_CELL';
-export const CHANGE_TURN = 'CHANGE_TURN';
+export const SET_WINNER = "SET_WINNER";
+export const CLICK_CELL = "CLICK_CELL";
+export const CHANGE_TURN = "CHANGE_TURN";
+export const RESET_GAME = "RESET_GAME";
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case SET_WINNER:
-            return {
-                ...state,
-                winner: action.winner,
-            }
-            case CLICK_CELL:
+  switch (action.type) {
+    case SET_WINNER:
+      return {
+        ...state,
+        winner: action.winner,
+      };
+    case CLICK_CELL:
+      const tableData = [...state.tableData]; // 리액트의 불변성.. 무조건 state를 그대로 쓰지말고 얕은 복사 (spread, 전개 문법) 한다.
+      tableData[action.row] = [...tableData[action.row]];
+      tableData[action.row][action.cell] = state.turn;
 
-            const tableData = [...state.tableData]; // 리액트의 불변성.. 무조건 state를 그대로 쓰지말고 얕은 복사 (spread, 전개 문법) 한다.
-            tableData[action.row] = [...tableData[action.row]];
-            tableData[action.row][action.cell] = state.turn;
-            
-            return {
-                ...state,
-                tableData,
-            };
+      return {
+        ...state,
+        tableData,
+        recentCell: [action.row, action.cell],
+      };
 
-            case CHANGE_TURN:
-                return {
-                    ...state,
-                    turn: state.turn === 'O' ? 'X' : 'O',
-                };
-    }
+    case CHANGE_TURN:
+      return {
+        ...state,
+        turn: state.turn === "O" ? "X" : "O",
+      };
+
+    case RESET_GAME:
+      return {
+        ...state,
+        turn: "O",
+        tableData: [
+          ["", "", ""],
+          ["", "", ""],
+          ["", "", ""],
+        ],
+        recentCell: [-1, -1],
+      };
+    default:
+      return state;
+  }
 };
 
 const TicTacToe = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { tableData, turn, winner, recentCell } = state;
+  // 순서 조심해라 디지기 전에
 
-    const [state, dispatch] = useReducer(reducer, initialState);
+  const onClickTable = useCallback(() => {
+    dispatch({ type: SET_WINNER, winner: "O" });
+  }, []);
 
-    const onClickTable = useCallback(()=>{
-        dispatch({ type: SET_WINNER, winner: 'O'});
-    },[]);
+  useEffect(() => {
+    const [row, cell] = recentCell;
 
-    return(
-        <>
-            <Table onClick={onClickTable} tableData={state.tableData} dispatch={dispatch}/>
-            {state.winner && <div>{state.winner}님의 승리!</div>}
-        </>
-    )
+    if (row < 0) {
+      return;
+    }
 
+    let win = false;
+
+    if (
+      tableData[row][0] === turn &&
+      tableData[row][1] === turn &&
+      tableData[row][2] === turn
+    ) {
+      win = true;
+    }
+    if (
+      tableData[0][cell] === turn &&
+      tableData[1][cell] === turn &&
+      tableData[2][cell] === turn
+    ) {
+      win = true;
+    }
+    if (
+      tableData[0][0] === turn &&
+      tableData[1][1] === turn &&
+      tableData[2][2] === turn
+    ) {
+      win = true;
+    }
+    if (
+      tableData[0][2] === turn &&
+      tableData[1][1] === turn &&
+      tableData[2][0] === turn
+    ) {
+      win = true;
+    }
+
+    if (win) {
+      // 이겼을 때
+      dispatch({ type: SET_WINNER, winner: turn });
+      dispatch({ type: RESET_GAME });
+    } else {
+      // 무승부시
+      let all = true;
+      tableData.forEach((row) => {
+        row.forEach((cell) => {
+          if (!cell) {
+            all = false;
+          }
+        });
+      });
+      if (all) {
+        dispatch({ type: RESET_GAME });
+      } else {
+        dispatch({ type: CHANGE_TURN });
+      }
+    }
+  }, [tableData]);
+  // 비동기로 작동하기 때문에, useEffect를 사용해서 state를 조작한다.
+  // useEffect는 기본적으로 처음 실행시에도 작동하기 때문에, if문을 사용해 최초 기본 상태일 때 return하도록 해줄 수 있다
+
+  return (
+    <>
+      <Table onClick={onClickTable} tableData={tableData} dispatch={dispatch} />
+      {winner && <div>{winner}님의 승리!</div>}
+    </>
+  );
 };
 
 export default TicTacToe;
